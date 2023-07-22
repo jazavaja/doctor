2756ce2ac58ae329f55a2b244e30ac9ead86d8d6
JavadSarlakHome <javadesmesh@gmail.com>
2023-07-22 18:56:02 +0430

    DOC-1 - updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,20 @@
 @extends('layout')
 
+@section('title')
+    لیست مقالات
+@endsection
 @section('content')
     <div class="container">
         <!-- Tabs -->
         <ul class="nav nav-tabs mb-3" id="myTabs" role="tablist">
             <li class="nav-item" role="presentation">
-                <a class="nav-link active" id="tab1-tab" data-toggle="tab" href="/thesis" role="tab" aria-controls="tab1" aria-selected="true">Thesis</a>
+                <a class="nav-link active" id="tab1-tab" data-toggle="tab" href="/thesis" role="tab" aria-controls="tab1" aria-selected="true">پایان نامه ها</a>
             </li>
             <li class="nav-item" role="presentation">
-                <a class="nav-link" id="tab2-tab" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="false">Proposal</a>
+                <a class="nav-link" id="tab2-tab" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="false">پروپزال ها</a>
             </li>
             <li class="nav-item" role="presentation">
-                <a class="nav-link" id="tab3-tab" data-toggle="tab" href="#tab3" role="tab" aria-controls="tab3" aria-selected="false">Plan</a>
+                <a class="nav-link" id="tab3-tab" data-toggle="tab" href="#tab3" role="tab" aria-controls="tab3" aria-selected="false">طرح ها</a>
             </li>
         </ul>
 
@@ -29,16 +32,27 @@
                     <table class="table table-bordered">
                         <thead>
                         <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
+                            <th>ردیف</th>
+                            <th>نام پدید اورنده</th>
+                            <th>عنوان پایان نامه</th>
+                            <th>جزییات</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
                             <td>1</td>
-                            <td>John Doe</td>
-                            <td>john@example.com</td>
+                            <td>علی محسنی</td>
+                            <td>بررسی هوش مصنوعی در علم دندان پزشکی</td>
+                            <td>
+                                <details>
+                                    <summary>مشاهده جزییات</summary>
+                                    <!-- Additional details content -->
+                                    <p>استاد راهنما : </p>
+                                    <p>استاد مشاور : </p>
+                                    <p>تاریخ دفاع : </p>
+                                    <p>تاریخ ثبت : </p>
+                                </details>
+                            </td>
                         </tr>
                         <!-- Add more rows here as needed -->
                         </tbody>
