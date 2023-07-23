af9e806cf65fe9575de4d3b997b2b30c34205669
JavadSarlakHome <javadesmesh@gmail.com>
2023-07-24 01:33:18 +0430

    DOC-1 - thesis

--- app/Http/Livewire/Admin/CreateThesisGroup.php
+++ app/Http/Livewire/Admin/CreateThesisGroup.php
@@ -3,9 +3,42 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Maatwebsite\Excel\Facades\Excel;
 
 class CreateThesisGroup extends Component
 {
+
+    use WithFileUploads;
+    public $file;
+
+    public function submit()
+    {
+        $this->validate([
+            'file' => 'required|file',
+        ]);
+
+
+        // Get the path of the uploaded file
+        $filePath = $this->file->getRealPath();
+
+        \Log::info($filePath);
+        // Read the contents of the Excel file using Maatwebsite\Excel
+        $data = Excel::toArray([], $filePath);
+
+        // $data will now contain an array representing the data in the Excel file
+        // Each element of the array corresponds to a row in the Excel file
+
+        // Process the data as needed
+        foreach ($data[0] as $row) {
+            \Log::info("AAA".$row);
+            // $row is an array representing the data of each row in the Excel file
+            // Do something with each row, e.g., display or manipulate the data
+        }
+
+        return redirect()->back()->with('success', 'Excel file read successfully.');
+    }
+
     public function render()
     {
         return view('livewire.admin.create-thesis-group');
--- resources/views/livewire/admin/create-thesis-group.blade.php
+++ resources/views/livewire/admin/create-thesis-group.blade.php
@@ -1,3 +1,13 @@
 <div>
     {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="form-group">
+        <label for="fileInput">Select Excel File</label>
+        <input type="file" wire:model="file" accept=".xlsx, .xls" class="form-control-file" id="fileInput" name="fileInput">
+    </div>
+
+
+    <button wire:click="submit" wire:loading.class="btn-loading" class="btn btn-primary">
+        <span wire:loading wire:target="submit">Please wait...</span>
+        <span wire:loading.remove wire:target="submit">Submit</span>
+    </button>
 </div>
