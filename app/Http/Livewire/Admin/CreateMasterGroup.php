7dff962f2388647daa9e4c0054c7c04feb5c93a0
JavadSarlakHome <javadesmesh@gmail.com>
2023-07-26 21:33:06 +0430

    DOC-proposal_Plan - Added masters

---
+++ app/Http/Livewire/Admin/CreateMasterGroup.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Imports\CategoryImport;
+use App\Imports\UsersImport;
+use App\Models\Category;
+use App\Models\User;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use Maatwebsite\Excel\Facades\Excel;
+
+class CreateMasterGroup extends Component
+{
+    use WithFileUploads;
+
+    public $file;
+    public $data = [];
+    protected $masters;
+
+    public function upload()
+    {
+        $this->validate([
+            'file' => 'required|mimes:xls,xlsx'
+        ]);
+
+        $path = $this->file->store('public');
+
+        $ff=storage_path('app/' . $path);
+
+
+        $import = new UsersImport();
+        Excel::import($import, $ff);
+
+// Get the total number of rows created
+        $rowCount = $import->getRowCountSuccess();
+        $fail = $import->getRowCountFail();
+
+    }
+
+    public function deleteMasters(){
+        User:: query()->delete();
+    }
+
+    public function render()
+    {
+        $this->masters=User::where('role','=',1)->take(20)->get();
+        return view('livewire.admin.create-master-group')->with('masters',$this->masters);
+    }
+}
